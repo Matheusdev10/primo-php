f5b79dc90b5f53bb0f43f4bb242228c3391c4b40
Matheus Campos <matheus_501@hotmail.com>
2022-06-20 12:55:37 -0300

    estudando numero primo php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div>
+
+<form method="get" action="primo.php">
+
+Número
+<input type="number">
+
+<button type="submit">É Primo?</button name="btn">
+
+
+
+</form>
+
+
+
+</div>
+    
+</body>
+</html>
---
+++ primo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+$n = isset($_GET{"num"}) ? $_GET{"num"} : null;
+// $button = isset($_GET{"btn"}) ? $_GET{"btn"} : null;
+
+
+
+echo "é primo";
+
+
+?>
+
+
+
+    
+</body>
+</html>
